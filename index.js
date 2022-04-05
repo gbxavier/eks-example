e35d723185ab3ae61cf50b38264d5e201899f610
Guilherme Xavier <guilherme.xavier@klarna.com>
2022-04-05 20:13:37 +0200

    Enhance HTML output

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 8080;
 // The APP!
 const app = express()
 app.get('/', (req, res) => {
-  let message = 'Welcome to 2022. '
-  message += `Hey, just a note... Your User Agent is "${req.get('User-Agent')}"`
+  let message = 'Welcome to 2022!'
+  message += `<br>Hey, just a note... Your User Agent is "${req.get('User-Agent')}"`
   res.send(message)
 })
 
